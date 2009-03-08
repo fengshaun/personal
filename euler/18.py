a42d9f1d3c7731ac4580eb58e19f00b1d190ad6f
Armin Moradi <amoradi@fedoraproject.org>
2009-03-07 22:12:37 -0400

    euler/18.py finished

--- euler/18.py
+++ euler/18.py
@@ -11,26 +11,37 @@ l = [                                       [75],
                         [53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14],
                       [70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57],
                     [91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48],
-                  [63, 66, 04, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31],
+                  [63, 66,  4, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31],
                 [ 4, 62, 98, 27, 23,  9, 70, 98, 73, 93, 38, 53, 60,  4, 23]]
 
-col = 0
-row = 0
-sum = 0
-
-sum += 75
-row += 1
-col += 1
-
-while row < len(l):
-    print l[row][col]
-    sum += l[row][col]
-    row += 1
+def update_element(row, col):
+    elem1 = 0
+    elem2 = 0
 
     try:
-        if l[row][col] < l[row][col + 1]:
-            col += 1
+        elem1 = l[row - 1][col - 1]
     except IndexError:
-        break
+        # it is the beginning element
+        l[row][col] += l[row - 1][col]
+        return
 
-print sum
+    try:
+        elem2 = l[row - 1][col]
+    except IndexError:
+        # it is the last element
+        l[row][col] += l[row - 1][col - 1]
+        return
+
+    if elem1 > elem2:
+        l[row][col] += elem1
+    else:
+        l[row][col] += elem2
+
+def update_row(row):
+    for col in range(len(l[row])):
+        update_element(row, col)
+
+for i in range(1, len(l)):
+    update_row(i)
+
+print max(l[-1])
